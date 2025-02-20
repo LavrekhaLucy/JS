2f59ace3b06324bf0346854b3ce3643cf59ec666
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-02-20 20:54:49 +0200

    Task 8 done

---
+++ Task_8/main.js
@@ -0,0 +1,56 @@
+// #AiN5CoUQ
+// Створити функцію, яка робить глибоку копію об'єкту.
+// Додати перевірки на undefined, null, NaN.
+//     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
+
+
+    Course = {
+    title: 'FullStack',
+    monthDuration: 7,
+    hourDuration: 909,
+    modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+     };
+
+
+    const CourseCopy = JSON.stringify(Course);
+    console.log(CourseCopy);
+
+    console.log(Course === CourseCopy);
+    console.log(Course.monthDuration === CourseCopy.monthDuration);
+    console.log(Course.monthDuration === null);
+    console.log(CourseCopy.age === undefined);
+
+    const parse=JSON.parse(CourseCopy);
+    console.log(parse)
+
+
+
+// #iz6emEsP2BA
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
+//     Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+
+    let coursesAndDurationArray = [
+        {title: 'JavaScript Complex', monthDuration: 5},
+        {title: 'Java Complex', monthDuration: 6},
+        {title: 'Python Complex', monthDuration: 6},
+        {title: 'QA Complex', monthDuration: 4},
+        {title: 'FullStack', monthDuration: 7},
+        {title: 'Frontend', monthDuration: 4}
+    ];
+
+    const reformArray = coursesAndDurationArray.map(function (value,index) {
+        return {id: index + 1,...value};
+    });
+
+    console.log(reformArray);
+
+    console.log(coursesAndDurationArray);
