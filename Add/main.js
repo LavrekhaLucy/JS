0b9a20437ccef041989f9d9e6b30ad0c2124aa8e
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-03-21 22:02:45 +0200

    change Add

--- Add/main.js
+++ Add/main.js
@@ -264,10 +264,11 @@ console.log(users [5]);
 //
 // – Створити цикл for на 10  ітерацій з кроком 1.
 // Вивести поточний номер кроку через console.log та document.write
-
-    for ( let i = 1; i<=10; i++){
-    console.log(i);
-    }
+//
+    for ( let i = 1; i<=10; i++) {
+        document.write(`<h1>${i}</h1>`)
+        console.log(i);
+            }
 
 
 //
@@ -276,80 +277,209 @@ console.log(users [5]);
 // – Створити цикл for на 100 ітерацій з кроком 1.
 // Вивести поточний номер кроку через console.log та document.write
 
-for ( let i = 1; i<=100; i++){
-    console.log(i);
-}
+    for ( let i = 1; i<=100; i++){
+        document.write(`<li>${i}</li>`)
+        console.log(i);
+    }
 
 
 
 // #s24slNyz7
 //
 // – Створити цикл for на 100 ітерацій з кроком 2.
-// Вивести поточний номер кроку через console.log та document.write
+// Вивести поточний номер кроку через console.log та document.write.
+
+    for ( let i = 2; i<=100; i++){
+        document.write(`<li>${i}</li>`)
+        console.log(i);
+    }
+
+
 //
 // #zananT5FR1
 //
 // – Створити цикл for на 100 ітерацій.
 // Вивести тільки парні кроки. через console.log + document.write
+        for ( let i = 0; i<=100; i++){
+            if (i % 2 === 0){
+            document.write(`<li>${i}</li>`)
+            console.log(i);
+        }
+        }
+
+
 //
 // #Tfrwls7FM
 //
-// – Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
-//
-//
-//
-//
-//
+// – Створити цикл for на 100 ітерацій.
+// Вивести тільки непарні кроки. через console.log + document.write
+            for ( let i = 0; i<=100; i++){
+            if (i % 2 !== 0){
+            document.write(`<li>${i}</li>`)
+            console.log(i);
+        }
+        }
+
+
 // #reLkOkTB29Q
-//
-// стоврити масив книжок (назва, кількість сторінок, автори , жанри).
-//
-// – знайти наібльшу книжку.
-//
+
+// створити масив книжок (назва, кількість сторінок, автори, жанри).
+let books = [
+    {
+    title: 'Charlie and the chocolate Factory',
+    pageCount: '240',
+    genre: ['Fantasy','Adventures'],
+    authors: [
+        {
+            name: 'Roald Dahl',
+            age: 45
+        },
+        {
+            name: 'Roald Dahl',
+            age: 45
+        }
+    ]
+},
+
+    {
+    title: 'Murder on the Orient Express',
+    pageCount: '240',
+    genre: 'Detective',
+    authors: [
+        {
+            name: 'Agatha Christie',
+            age: 86
+        }
+    ]
+},
+
+    {
+    title: 'Moby-Dick',
+    pageCount: '672',
+    genre: 'Adventures',
+    authors: [
+        {
+            name: 'Herman Melville',
+            age: 58
+        },
+        {
+            name: 'Herman Melville',
+            age: 58
+        },
+        {
+            name: 'Herman Melville',
+            age: 58
+        }
+    ]
+}
+            ]
+        console.log(books);
+
+// – знайти найбільшу книжку.
+
+        console.log(books[2].pageCount);
+
 // – знайти книжку/ки з найбільшою кількістю жанрів
-//
+
+       console.log(books[0].genre);
+
 // – знайти книжку/ки з найдовшою назвою
-//
-// – знайти книжку/ки які писали 2 автори
-//
+     console.log(books[0].title);
+
+     // – знайти книжку/ки які писали 2 автори
+    console.log(books[0].authors);
+
+
 // – знайти книжку/ки які писав 1 автор
-//
-//
-//
+    console.log(books[1].authors);
+
+
 // 1. Створити пустий масив та :
-//
 //     a. заповнити його 50 парними числами за допомоги циклу.
+        let emptyArray =[];
+        let start = 100;
+
+        for(start; start>0; start--){
+           emptyArray.push(start--);
+        }
+        console.log(emptyArray);
+
+
 //
 //     b. заповнити його 50 непарними числами за допомоги циклу.
+
+        let result =[];
+        let startnon = 101;
+
+        for(startnon; startnon>1; startnon--){
+            result.push(startnon--);
+        }
+        console.log(result);
+
 //
-//     c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+//   c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+
+        const getRandom = (start = 1, end = 100) => {
+            return Math.floor(Math.random() * (end - start + 1) + start);
+        };
+
+        let randomArr = [];
+
+        for (let i= 0; i< 20; i++) {
+          randomArr.push(getRandom());
+        }
+        console.log(randomArr);
+
 //
 // d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+
+
+        const randomNumber = (start = 8, end = 732) =>{
+            return Math.floor (Math.random() * (end - start +1) + start);
+        };
+        let randomNumbers =[];
+
+        for (let i =0; i < 20; i++){
+            randomNumbers.push(randomNumber());
+        }
+        console.log(randomNumbers);
+
+
+// 2. Вивести за допомогою console.log кожен третій елемент
+
+    for (let i=0; i < emptyArray.length; i++){
+    if (i % 3 === 0){
+
+        console.log(i);
+    }
+}
+
 //
-// 2. Вивести за допомогою console.log кожен третій елемен
+// 3. Вивести за допомогою console.log кожен третій елемент тільки якщо цей елемент є парним.
+
 //
-// 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
-//
-// 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
+// 4. Вивести за допомогою console.log кожен третій елемент тільки якщо цей елемент є парним та
+//    записати їх в новий масив
 //
 // 5. Вивести кожен елемент масиву, сусід справа якого є парним
 //
 // EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
 //
-// 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
+// 6. Є масив з числами [100,250,50,168,120,345,188],
+// Які характеризують вартість окремої покупки. Обрахувати середній чек.
 //
 //
 //
-// 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+// 7. Створити масив з рандомними значеннями,
+// помножити всі його елементи на 5 та перемістити їх в інший масив.
+
+
+
+// 8. Створити масив з будь якими значеннями (стрінги, числа, і тд…).
+// пройтись по ньому, і якщо елемент є числом – додати його в інший масив.
 //
-// 8. Створити масив з будь якими значеннями (стрінги, числа, і тд…). пройтись по ньому, і якщо елемент є числом – додати його в інший масив.
-//
-//
-//
-//
-//
-//
-//
+
+
 // – Дано 2 масиви з рівною кількістю об’єктів.
 //
 //     Масиви:
@@ -388,47 +518,31 @@ for ( let i = 1; i<=100; i++){
 //
 // Example:
 //
-//     let usersWithCities = [
+//         let usersWithCities = [
 //
-//         {
+//             {
+//                 id: 1, // <===
+//                 name: ‘vasya’,
+//                  age: 31,
+//                  status:false,
+//                 address:{
+//                  user_id: 1, // <===
+//                  country: 'Ukraine',
+//                  city: 'Ternopil'}
+//                  }
+//                 TO BE CONTINUED …..]
+
+
 //
-//             id: 1, // <===
-//
-//             name: ‘vasya’,
-//
-// age: 31,
-//
-//     status: false,
-//
-//     address: {
-//
-//     user_id: 1, // <===
-//
-//         country: ‘Ukraine’,
-//
-//     city: ‘Ternopil’
-//
-// }
-//
-// },
-//
-// // TO BE CONTINUED …..
-//
-// ]
+// – Взяти масив з 10 чисел або створити його.
+// Вивести в консоль тільки ті елементи, значення яких є парними.
+
+
+
 //
 //
-//
-//
-//
-//
-//
-//
-//
-// – Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
-//
-//
-//
-// – Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+// – Взяти масив з 10 чисел або створити його.
+// Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
 //
 //
 //
