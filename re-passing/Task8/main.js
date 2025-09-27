6c45e73c8e5b19cb92f8a72bfa6539361ee31471
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-09-27 15:18:51 +0300

    повторне проходження матеріалу(8)

---
+++ re-passing/Task8/main.js
@@ -0,0 +1,41 @@
+// #iz6emEsP2BA
+//
+// – є масив
+//
+// let coursesAndDurationArray = [
+//
+//     {title: ‘JavaScript Complex’, monthDuration: 5},
+//
+// {title: ‘Java Complex’, monthDuration: 6},
+//
+// {title: ‘Python Complex’, monthDuration: 6},
+//
+// {title: ‘QA Complex’, monthDuration: 4},
+//
+// {title: ‘FullStack’, monthDuration: 7},
+//
+// {title: ‘Frontend’, monthDuration: 4}
+//
+// ];
+//
+// за допомогою map перетворити кожен елемент на наступний тип {id,title,monthDuration
+//
+//     Зробити все ВИКЛЮЧНО за допомогою інлайн конструкції
+//
+//
+
+
+
+
+
+//
+//
+//
+//     #AiN5CoUQ
+//
+//     Створити функцію, яка робить глибоку копію об’єкта.
+//
+//         Додати перевірки на undefined, null, NaN.
+//
+//         Подумати і реалізувати логіку, в якій кінцевий об’єкт буде мати функції, які в нього були до цього моменту.
+//
