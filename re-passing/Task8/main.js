f3361e8930bd7e44be5e8775bdb170a39ff030e7
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-09-30 20:49:36 +0300

    повторне проходження матеріалу(8)

--- re-passing/Task8/main.js
+++ re-passing/Task8/main.js
@@ -24,7 +24,17 @@
 //
 //
 
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+ let newArr= coursesAndDurationArray.map( (value,index)=> ({id: index + 1, ...value}));
 
+console.log(newArr);
 
 
 
